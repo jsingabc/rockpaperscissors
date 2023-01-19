46d73337bb96db6b4c64333fd0622292c7b7c528
Jermain Singleton <jsingabc@gmail.com>
2023-01-19 15:39:04 -0500

    Made a function to run game once and get a result playRound

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "player wins";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") { 
+    return "cpu wins";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "cpu wins";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "player wins";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "cpu wins";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "player wins";
+  } else {
+    return "draw";
+  }
+}
+  console.log(playRound(playerSelection, computerSelection));
+
+  
